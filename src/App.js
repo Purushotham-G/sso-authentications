381a89562acfaa70bda9106256cae6f047f34a9e
purushotham <purushu1995@gmail.com>
2023-12-08 12:02:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React testing
+          
         </a>
       </header>
     </div>
