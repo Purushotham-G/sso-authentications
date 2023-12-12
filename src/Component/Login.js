11f815a218ca24d79faf05268445ee769d84bdc0
purushotham <purushu1995@gmail.com>
2023-12-12 15:11:42 +0530

    login form ui

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,3 +1,4 @@
+import { Button, Grid, Stack, TextField, Typography } from '@mui/material';
 import React from 'react'
 import { useForm } from 'react-hook-form';
 
@@ -6,21 +7,65 @@ const Login = () => {
     const onSubmit = (data) => console.log(data);
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input 
+
+    {/* <div>
+        <div className='grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2'>
+            <div>1</div>
+            <div className='col-span-3'>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+        </div>
+    </div> */}
+    <Stack sx={{}}>
+        <Stack sx={{p:4, }}>
+            <Grid container>
+                <Grid item xs={12} lg={6} md={6} sm={6}>
+                    <Stack>
+                    <img src='../../../Assets/images/lock.jpg' alt='image not found' height="auto"  />
+                    </Stack>
+                </Grid>
+                <Grid item xs={12} lg={6} md={6} sm={6}>
+                    <Stack sx={{gap:2, p:4}}>
+                        <Stack>
+                            <Typography sx={{fontSize:"32px"}}>Login</Typography>
+                        </Stack>
+                    <Stack sx={{}}>
+                        <Stack sx={{alignItems:"flex-start"}}>
+                          <Typography>UserName</Typography>
+                        </Stack>  
+                        <TextField fullWidth />
+                    </Stack>
+                    <Stack sx={{}}>
+                        <Stack sx={{alignItems:"flex-start"}}>
+                          <Typography >Password</Typography>
+                        </Stack>
+                    <TextField  />
+                    </Stack>
+                    <Button>Submit</Button>
+                    </Stack>
+                </Grid>
+            </Grid>
+        </Stack>
+    </Stack>
+    {/* <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField 
         {...register("firstName", { required: true })} 
         aria-invalid={errors.firstName ? "true" : "false"} 
       />
-      {errors.firstName?.type === 'required' && <p role="alert">First name is required</p>}
+      {errors.firstName?.type === 'required' && <p role="alert" className='text-red-500'>First name is required</p>}
 
-      <input 
+      <TextField 
         {...register("mail", { required: "Email Address is required" })} 
         aria-invalid={errors.mail ? "true" : "false"} 
       />
-      {errors.mail && <p role="alert">{errors.mail?.message}</p>}
+      {errors.mail && <p role="alert" className='text-red-500'>{errors.mail?.message}</p>}
       
-      <input type="submit" />
-    </form>
+      <button type={onSubmit}></button>
+    </form> */}
     </>
   )
 }
